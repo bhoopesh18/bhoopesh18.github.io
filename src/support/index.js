7ca1a54014933757fbc839c48b57df88d7ff1955
bhoopesh18 <30494108+bhoopesh18@users.noreply.github.com>
2024-07-31 20:02:52 +0530

    Create index.js

---
+++ src/support/index.js
@@ -0,0 +1,19 @@
+export const DubuCry = [
+  "/gifs/crying-dudu.gif",
+  "/gifs/dudu-crying2.gif",
+  "/gifs/crying-dudu3.gif",
+  "/gifs/crying-dudu4.gif",
+];
+
+export const Sorries = [
+  "Noo😒😠",
+  "Think again 🥺",
+  "again again 🥺",
+  "Really ?? 🥺",
+  "please ??",
+  "are you serious😣😖",
+  "is it true ?",
+  "seriouslyyy ?",
+  "🥺",
+  "pleaseezz🤕",
+];
